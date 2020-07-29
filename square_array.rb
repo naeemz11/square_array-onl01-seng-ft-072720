5b63246ce884acdbd401d73a07fafa788c3151e3
Zannatul Naeem <mollahnaeem@gmail.com>
2020-07-29 19:52:52 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 array = [1,2,3]
 
 def square_array(array)
@@ -6,7 +6,7 @@ def square_array(array)
  array.each do |num|
    new_array << (num**2)
  end
- return new_array
- binding.pry 
+ puts new_array
+ 
 end
 
